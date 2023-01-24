fda8b502ac988e0c4cff8a06dd73949ac5d43e56
Zack MCMAHON <>
2023-01-24 17:22:05 +0000

    Added print statement

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,7 @@ import java.awt.*;
 public class Main{
 
     public static void main(String[] args){
+      System.out.println("I'm on replit!");
         JFrame myFrame = new JFrame();
        // DrawPanel myPanel = new DrawPanel();
         BallPanel ball = new BallPanel();
